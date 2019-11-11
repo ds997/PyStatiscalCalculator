3bde9da9c5794593adf49add796cc204e9ae6fc3
soham-banerjeee <banerjeesoham04@gmail.com>
2019-11-11 15:37:19 -0500

    Fix: resolved standardized score

--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -106,6 +106,21 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(round(self.Statistics.sample_mean(list1)), round(result_test))
 
+    def standardized_score(self):
+
+        test_data = CsvReader('Tests/Data/population_list.csv').data
+        test_result = CsvReader('Tests/Data/result_data.csv').data
+
+        for column in test_result:
+            result_test = float(column['Standardized Score'])
+
+        list1 = []
+
+        for row in test_data:
+            result = float(row['List1'])
+            list1.append(result)
+
+        self.assertEqual(round(self.Statistics.standardized_score(list1)), round(result_test))
 
 if __name__ == '__main__':
     unittest.main()
