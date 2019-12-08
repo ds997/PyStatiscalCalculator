cb6f70377ef8de58f3c137b5734a6283abf00fd8
Divyanshu Sachdeva <ds997@njit.edu>
2019-12-08 15:31:43 -0500

    Feature: Added code for Customer model class

---
+++ Database/create_model.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, Integer, String, Column, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
+
+engine = create_engine("sqlite:///db1.sqlite")
+Base = declarative_base()
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(50), nullable=False)
+    email = Column(String(200), nullable=False)
+    address = Column(String(200), nullable=False)
+    town = Column(String(200), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    orders = relationship("Order", backref='customer')
